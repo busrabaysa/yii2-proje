7de95e338d344c2702029e86f35d0406b0df94fe
busrabaysa <74297910+busrabaysa@users.noreply.github.com>
2021-01-17 08:33:38 +0300

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,8 @@
+<?php
+namespace busrabaysa\hospital;
+
+class Module extends \yii\base\Module
+{
+    public $captcha = false; //show captcha
+    public $adminRoles = ['@']; //['superadmin', 'administrator', 'admin']; //roles who can see orders
+}
---
+++ controllers/ActionController.php
@@ -0,0 +1,59 @@
+<?php
+namespace busrabaysa\hospital\controllers;
+
+use busrabaysa\hospital\models\ActionForm;
+use busrabaysa\hospital\models\ActionSearch;
+use yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+class ActionController extends Controller
+{
+    public $layout = 'hospital';
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['index'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => $this->module->adminRoles,
+                    ]
+                ],
+            ],
+        ];
+    }
+
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        $model = new ActionSearch;
+
+        return $this->render('index', [
+            'dataProvider' => $model->search(Yii::$app->request->queryParams),
+            'searchModel' => $model,
+        ]);
+    }
+
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => ActionForm::findOne($id)
+        ]);
+    }
+}
